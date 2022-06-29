33f9bab35614199848f452dd69e3da6db7cc45e4
classicfurniture <classicfurniture067@gmail.com>
2022-06-30 01:49:11 +0530

    First Package V1.0.4

--- index.js
+++ index.js
@@ -6,11 +6,5 @@ module.exports = {
     subnum: (num, subby) => {
         const subtraction = num - subby;
         return subtraction;
-    },
-    fs2remid: (element) => {
-        document.getElementById(element).style.fontSize = '2rem';
-    },
-    fs2remclass: (element) => {
-        document.getElementsByClassName(element).style.fontSize = '2rem';
     }
 }
